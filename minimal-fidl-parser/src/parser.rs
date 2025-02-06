babe9d43ff975e15713623ab69de7b1184dfbaa0
TimMeiwald <tim.meiwald@gmail.com>
2025-02-06 09:01:14 +0200

    Regenerated parser

--- minimal-fidl-parser/src/parser.rs
+++ minimal-fidl-parser/src/parser.rs
@@ -361,7 +361,7 @@ pub fn wildcard<T: Context>(parent: Key, context: &RefCell<T>, source: &Source, 
 pub fn package<T: Context>(parent: Key, context: &RefCell<T>, source: &Source, position: u32) -> (bool, u32) {
 	// Describes the package import
 	let closure_1 = _string_terminal_opt_ascii(&[b'p',b'a',b'c',b'k',b'a',b'g',b'e']);
-	let closure_2 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_2 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_3 = _sequence(&closure_1, &closure_2);
 	let closure_4 = _var_name(Rules::type_ref, context, type_ref);
 	let closure_5 = _sequence(&closure_3, &closure_4);
@@ -373,17 +373,17 @@ pub fn package<T: Context>(parent: Key, context: &RefCell<T>, source: &Source, p
 pub fn import_namespace<T: Context>(parent: Key, context: &RefCell<T>, source: &Source, position: u32) -> (bool, u32) {
 
 	let closure_1 = _string_terminal_opt_ascii(&[b'i',b'm',b'p',b'o',b'r',b't']);
-	let closure_2 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_2 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_3 = _sequence(&closure_1, &closure_2);
 	let closure_4 = _var_name(Rules::type_ref, context, type_ref);
 	let closure_5 = _sequence(&closure_3, &closure_4);
 	let closure_6 = _var_name(Rules::wildcard, context, wildcard);
 	let closure_7 = _sequence(&closure_5, &closure_6);
-	let closure_8 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_8 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_9 = _sequence(&closure_7, &closure_8);
 	let closure_10 = _string_terminal_opt_ascii(&[b'f',b'r',b'o',b'm']);
 	let closure_11 = _sequence(&closure_9, &closure_10);
-	let closure_12 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_12 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_13 = _sequence(&closure_11, &closure_12);
 	let closure_14 = _var_name(Rules::file_path, context, file_path);
 	let closure_15 = _sequence(&closure_13, &closure_14);
@@ -395,11 +395,11 @@ pub fn import_namespace<T: Context>(parent: Key, context: &RefCell<T>, source: &
 pub fn import_model<T: Context>(parent: Key, context: &RefCell<T>, source: &Source, position: u32) -> (bool, u32) {
 
 	let closure_1 = _string_terminal_opt_ascii(&[b'i',b'm',b'p',b'o',b'r',b't']);
-	let closure_2 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_2 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_3 = _sequence(&closure_1, &closure_2);
 	let closure_4 = _string_terminal_opt_ascii(&[b'm',b'o',b'd',b'e',b'l']);
 	let closure_5 = _sequence(&closure_3, &closure_4);
-	let closure_6 = move |parent: Key, source: &Source, position: u32| ws(parent, context, source, position);
+	let closure_6 = move |parent: Key, source: &Source, position: u32| ws_atlone(parent, context, source, position);
 	let closure_7 = _sequence(&closure_5, &closure_6);
 	let closure_8 = _var_name(Rules::file_path, context, file_path);
 	let closure_9 = _sequence(&closure_7, &closure_8);
