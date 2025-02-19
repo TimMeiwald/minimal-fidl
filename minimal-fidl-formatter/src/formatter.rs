2d1efdb552c2cb1ec09940e67522835580ab6cd7
TimMeiwald <tim.meiwald@gmail.com>
2025-02-19 15:51:20 +0200

    Fixed bug in assert

--- minimal-fidl-formatter/src/formatter.rs
+++ minimal-fidl-formatter/src/formatter.rs
@@ -927,7 +927,7 @@ impl<'a> Formatter<'a> {
                 }
             }
         }
-        debug_assert_eq!(ret_vec.len(), 0, "We do not expect input_params to ever return empty.");
+        debug_assert!(ret_vec.len() != 0, "We do not expect input_params to ever return empty.");
         ret_vec
     }
 
